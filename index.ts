bceea0ddd7984d7a69257046202dbc7a9e27effa
Denis Carriere <carriere.denis@gmail.com>
2023-12-09 18:30:52 -0500

    only log error

--- index.ts
+++ index.ts
@@ -32,11 +32,11 @@ function addToQueue() {
             const response = await session.transact({actions});
             if ( response.response ) {
                 const trx_id = response.response.transaction_id;
-                console.info(`EOS\t${new Date().toISOString()}\t${trx_id}`);
+                // console.info(`EOS\t${new Date().toISOString()}\t${trx_id}`);
                 const { action_traces } = response.response.processed;
                 for ( const action_trace of action_traces ) {
                     const rlptx = action_trace.act.data.rlptx;
-                    if ( rlptx ) console.info(`EVM\t${new Date().toISOString()}\t${toTransactionId(rlptx)}`);
+                    // if ( rlptx ) console.info(`EVM\t${new Date().toISOString()}\t${toTransactionId(rlptx)}`);
                 }
             }
         } catch (e) {
