38b9de9d4d06d85c996548a943a7581d22573dc7
Denis Carriere <carriere.denis@gmail.com>
2023-12-29 13:18:04 -0500

    update loggin

--- src/transact.ts
+++ src/transact.ts
@@ -7,13 +7,13 @@ export async function transact(session: Session, actions: AnyAction[]) {
     if ( response.response ) {
         const trx_id = response.response.transaction_id;
         if (VERBOSE) {
-            console.info(`EOS\t${new Date().toISOString()}\t${trx_id}`);
+            console.info(`EOS\t${new Date().toISOString()}\thttps://eos.eosq.eosnation.io/tx/${trx_id}`);
         }
         const { action_traces } = response.response.processed;
         for ( const action_trace of action_traces ) {
             const rlptx = action_trace.act.data.rlptx;
             if ( rlptx && VERBOSE ) {
-                console.info(`https://explorer.evm.eosnetwork.com/tx/${toTransactionId(rlptx)}`);
+                console.info(`EVM\t${new Date().toISOString()}\thttps://explorer.evm.eosnetwork.com/tx/${toTransactionId(rlptx)}`);
             }
         }
     }
